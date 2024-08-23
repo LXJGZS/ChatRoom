f7a0623a570b39982d8f6eca002c9599b9e09c4e
LXJ <ugq_it86prxkz@dingtalk.com>
2024-08-24 07:28:56 +0800

    安全更新

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const publicIp = require('public-ip');
+const os = require('os');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,18 +12,29 @@ app.use(express.static('public'));
 
 const rooms = {};
 
-io.on('connection', (socket) => {
-    console.log('用户已连接，等待IP信息...');
+function getLocalIp() {
+    const interfaces = os.networkInterfaces();
+    for (const devName in interfaces) {
+        const iface = interfaces[devName];
+        for (let i = 0; i < iface.length; i++) {
+            const alias = iface[i];
+            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+                return alias.address;
+            }
+        }
+    }
+    return '0.0.0.0';
+}
 
-    socket.on('reportIP', (ips) => {
-        socket.localIP = ips.local || 'Unknown';
-        socket.publicIP = ips.public || 'Unknown';
-        console.log(`用户IP地址: 局域网 ${socket.localIP}, 公网 ${socket.publicIP}`);
-    });
+const localIp = getLocalIp();
+
+io.on('connection', async (socket) => {
+    const clientPublicIp = await publicIp.v4();
+    console.log(`用户已连接，局域网IP地址: ${localIp}, 公网IP地址: ${clientPublicIp}`);
 
     socket.on('login', (username) => {
         socket.username = username;
-        console.log(`用户 ${username} 登录，IP地址: 局域网 ${socket.localIP}, 公网 ${socket.publicIP}`);
+        console.log(`用户 ${username} 登录，局域网IP地址: ${localIp}, 公网IP地址: ${clientPublicIp}`);
     });
 
     socket.on('createRoom', (room) => {
@@ -47,7 +60,7 @@ io.on('connection', (socket) => {
         socket.room = room;
         io.to(room).emit('playerJoined', { username: socket.username, players: rooms[room].players });
         socket.emit('joinedRoom', room);
-        console.log(`用户 ${socket.username} 加入房间 ${room}，IP地址: 局域网 ${socket.localIP}, 公网 ${socket.publicIP}`);
+        console.log(`用户 ${socket.username} 加入房间 ${room}，局域网IP地址: ${localIp}, 公网IP地址: ${clientPublicIp}`);
     }
 
     socket.on('chat', (data) => {
@@ -58,7 +71,7 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        console.log(`用户已断开连接，IP地址: 局域网 ${socket.localIP}, 公网 ${socket.publicIP}`);
+        console.log(`用户已断开连接，局域网IP地址: ${localIp}, 公网IP地址: ${clientPublicIp}`);
         if (socket.room && rooms[socket.room]) {
             rooms[socket.room].players = rooms[socket.room].players.filter(player => player !== socket.username);
             io.to(socket.room).emit('playerLeft', { username: socket.username, players: rooms[socket.room].players });
@@ -80,6 +93,7 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`服务器运行在端口 ${PORT}`);
+server.listen(PORT, async () => {
+    const serverPublicIp = await publicIp.v4();
+    console.log(`服务器运行在端口 ${PORT}，局域网IP地址: ${localIp}, 公网IP地址: ${serverPublicIp}`);
 });
