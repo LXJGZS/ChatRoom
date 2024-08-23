260bd2a17a912912a192f0f061e51fee7ce0a604
LXJ <ugq_it86prxkz@dingtalk.com>
2024-08-24 07:49:50 +0800

    安全修复更新

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const https = require('https');
 const socketIo = require('socket.io');
 
 const app = express();
@@ -10,9 +11,53 @@ app.use(express.static('public'));
 
 const rooms = {};
 
+function getClientIp(socket) {
+    const ipAddress = socket.handshake.headers['x-forwarded-for'] || 
+                      socket.handshake.headers['x-real-ip'] ||
+                      socket.handshake.address;
+    
+    // 如果是 IPv6 格式，我们只取 IPv4 部分
+    return ipAddress.split(',')[0].trim().replace(/^::ffff:/, '');
+}
+
+function verifyIpFormat(ip) {
+    // 简单的 IPv4 格式验证
+    return /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ip);
+}
+
+function getPublicIp(callback) {
+    https.get('https://api.ipify.org?format=json', (resp) => {
+        let data = '';
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+        resp.on('end', () => {
+            try {
+                const ip = JSON.parse(data).ip;
+                callback(null, ip);
+            } catch (error) {
+                callback(error, null);
+            }
+        });
+    }).on("error", (err) => {
+        callback(err, null);
+    });
+}
+
 io.on('connection', (socket) => {
-    // 获取用户的 IP 地址
-    const clientIp = socket.handshake.headers['x-forwarded-for'] || socket.handshake.address;
+    let clientIp = getClientIp(socket);
+
+    if (!verifyIpFormat(clientIp) || clientIp === '127.0.0.1' || clientIp === '::1') {
+        getPublicIp((err, ip) => {
+            clientIp = err ? "无法获取IP" : ip;
+            handleConnection(socket, clientIp);
+        });
+    } else {
+        handleConnection(socket, clientIp);
+    }
+});
+
+function handleConnection(socket, clientIp) {
     console.log(`用户已连接，公网IP地址: ${clientIp}`);
 
     socket.on('login', (username) => {
@@ -24,18 +69,18 @@ io.on('connection', (socket) => {
         if (!rooms[room]) {
             rooms[room] = { players: [] };
         }
-        joinRoom(socket, room);
+        joinRoom(socket, room, clientIp);
     });
 
     socket.on('joinRoom', (room) => {
         if (rooms[room]) {
-            joinRoom(socket, room);
+            joinRoom(socket, room, clientIp);
         } else {
             socket.emit('roomNotFound');
         }
     });
 
-    function joinRoom(socket, room) {
+    function joinRoom(socket, room, ip) {
         socket.join(room);
         if (!rooms[room].players.includes(socket.username)) {
             rooms[room].players.push(socket.username);
@@ -43,7 +88,7 @@ io.on('connection', (socket) => {
         socket.room = room;
         io.to(room).emit('playerJoined', { username: socket.username, players: rooms[room].players });
         socket.emit('joinedRoom', room);
-        console.log(`用户 ${socket.username} 加入房间 ${room}，公网IP地址: ${clientIp}`);
+        console.log(`用户 ${socket.username} 加入房间 ${room}，公网IP地址: ${ip}`);
     }
 
     socket.on('chat', (data) => {
@@ -67,13 +112,13 @@ io.on('connection', (socket) => {
     socket.on('rejoinRoom', (data) => {
         if (rooms[data.room]) {
             socket.username = data.username;
-            joinRoom(socket, data.room);
+            joinRoom(socket, data.room, clientIp);
             socket.emit('rejoinedRoom');
         } else {
             socket.emit('roomNotFound');
         }
     });
-});
+}
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
